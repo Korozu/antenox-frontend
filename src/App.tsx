572a21a5d21d74f1d010b688522ccb47b93ea9ec
delaval.r <remi.delaval.partner@decathlon.com>
2026-05-02 20:45:17 +0200

    refactor: update App component to use StrictMode and render a deployment success message

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Routes } from '@generouted/react-router'
-import { BrowserRouter } from 'react-router-dom'
+// main.tsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-export default function App() {
-  return (
-    <BrowserRouter basename="/antenox-frontend">
-      <Routes />
-    </BrowserRouter>
-  )
-}
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <div style={{ background: 'red', color: 'white', padding: '50px', fontSize: '30px' }}>
+            SI TU VOIS CE MESSAGE ROUGE, LE DEPLOIEMENT FONCTIONNE.
+        </div>
+    </StrictMode>,
+)
