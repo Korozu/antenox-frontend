aa4d9291a3dd38b03a26d09b2fb7ba2382b247e5
delaval.r <remi.delaval.partner@decathlon.com>
2026-05-02 20:52:00 +0200

    refactor: remove BrowserRouter from App component and simplify rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { Routes } from '@generouted/react-router'
-import { BrowserRouter } from 'react-router-dom'
 
 export default function App() {
-  return (
-    <BrowserRouter basename="/antenox-frontend">
-      <Routes />
-    </BrowserRouter>)
+  return <Routes />
 }
