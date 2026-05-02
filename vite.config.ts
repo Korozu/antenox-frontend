ebaff8f35687e1637def2d4b275e3762ca1e681a
delaval.r <remi.delaval.partner@decathlon.com>
2026-05-02 20:41:41 +0200

    feat: add HTML transformation for GitHub Pages support in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,28 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     generouted(),
+    {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</head>',
+          `  <!-- Start Single Page Apps for GitHub Pages -->
+    <script type="text/javascript">
+      (function(l) {
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) { 
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location))
+    </script>
+    <!-- End Single Page Apps for GitHub Pages -->
+  </head>`
+        )
+      }
+    }
   ],
 })
