bee6feae08edadee642d19e62c1f86c6e563b6c6
delaval.r <remi.delaval.partner@decathlon.com>
2026-05-02 20:46:58 +0200

    refactor: restructure App component to use BrowserRouter and remove deployment message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-// main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Routes } from '@generouted/react-router'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <div style={{ background: 'red', color: 'white', padding: '50px', fontSize: '30px' }}>
-            SI TU VOIS CE MESSAGE ROUGE, LE DEPLOIEMENT FONCTIONNE.
-        </div>
-    </StrictMode>,
-)
+export default function App() {
+  return (
+    <BrowserRouter basename="/antenox-frontend">
+      <Routes />
+    </BrowserRouter>)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <div style={{ background: 'red', color: 'white', padding: '50px', fontSize: '30px' }}>
+            SI TU VOIS CE MESSAGE ROUGE, LE DEPLOIEMENT FONCTIONNE.
+        </div>
+    </StrictMode>,
 )
