7269995ac7e166b39010a9731a975370fccb7f31
delaval.r <remi.delaval.partner@decathlon.com>
2026-05-02 20:33:23 +0200

    fix: update App component to remove BrowserRouter and adjust index.html for French localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from 'react-router-dom'
 import { Routes } from '@generouted/react-router'
 
 export default function App() {
-  return (
-    <BrowserRouter basename="/antenox-frontend">
-      <Routes />
-    </BrowserRouter>
-  )
+  return <Routes />
 }
